c2b9d6316d15d70b86c9c20a1b1d1083b0288b62
AlirezA <bojbaj@gmail.com>
2018-05-11 13:54:59 +0430

    media_code length fix in db

--- db/4-create-media-table.sql
+++ db/4-create-media-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE `tInstaMedia` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `insta_id` varchar(10) COLLATE utf8_persian_ci NOT NULL,
   `media_id` varchar(19) COLLATE utf8_persian_ci NOT NULL,
-  `shortcode` varchar(20) COLLATE utf8_persian_ci NOT NULL,
+  `shortcode` varchar(11) COLLATE utf8_persian_ci NOT NULL,
   `tInstaHashtag_id` int(11) NULL,
   `is_video` tinyint(4) NOT NULL,
   `taken_at_timestamp` INT NOT NULL,
